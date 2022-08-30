8c51bf1866b413dce30af6f8f426661dc4c2c3fe
LMartin1998 <jlmh_aguila@hotmail.com>
2022-08-30 12:43:47 -0500

    Cambios pre upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const button_primary = document.querySelector('#button1');
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.modal__content--close');
 
+const menu_hamburger = document.querySelector('.hamburger_menu');
+const menu__content  = document.querySelector('.mobile-menu__content');
+
 button_primary.addEventListener('click', () => {
     modal.classList.remove('hidden');
     modal.classList.add('visible');
@@ -11,3 +14,9 @@ closeButton.addEventListener('click', () =>{
     modal.classList.add('hidden');
     modal.classList.remove('visible');
 })
+
+menu_hamburger.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu() {
+    menu__content.classList.toggle('hidden');
+}
